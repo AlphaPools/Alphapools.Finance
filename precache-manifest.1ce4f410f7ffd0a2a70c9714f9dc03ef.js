f27504ae101bb8cb8c2c3e6b4aa0d1dde1407d30
owner <user1@Users-MBP.broadband>
2020-12-10 03:18:33 +0000

    Updates

---
+++ precache-manifest.1ce4f410f7ffd0a2a70c9714f9dc03ef.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca85a797e74ae6047de1ce7c6642511",
+    "url": "/Alphapools.Finance/index.html"
+  },
+  {
+    "revision": "39c9b08caa24d3243ee7",
+    "url": "/Alphapools.Finance/static/css/3.2a86faee.chunk.css"
+  },
+  {
+    "revision": "96689ccaf7a747aacccc",
+    "url": "/Alphapools.Finance/static/css/main.4cbc5b6b.chunk.css"
+  },
+  {
+    "revision": "8aab5018d61193830be9",
+    "url": "/Alphapools.Finance/static/js/0.1b10c4b8.chunk.js"
+  },
+  {
+    "revision": "214fbc7f3d7ebfaed71d",
+    "url": "/Alphapools.Finance/static/js/10.4403ea7c.chunk.js"
+  },
+  {
+    "revision": "39c9b08caa24d3243ee7",
+    "url": "/Alphapools.Finance/static/js/3.7eb206c4.chunk.js"
+  },
+  {
+    "revision": "f079e55058b9950cbb7b3ebef380aba4",
+    "url": "/Alphapools.Finance/static/js/3.7eb206c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4b1fef7eaa8ee49c20",
+    "url": "/Alphapools.Finance/static/js/4.d57231bf.chunk.js"
+  },
+  {
+    "revision": "e538d3fe5427bd84e944",
+    "url": "/Alphapools.Finance/static/js/5.9686506c.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/Alphapools.Finance/static/js/5.9686506c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588fec69d420af64521f",
+    "url": "/Alphapools.Finance/static/js/6.cc8b85dc.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/Alphapools.Finance/static/js/6.cc8b85dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508a721e9cbef4fcc039",
+    "url": "/Alphapools.Finance/static/js/7.799b8915.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Alphapools.Finance/static/js/7.799b8915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1277e53427404647d0",
+    "url": "/Alphapools.Finance/static/js/8.4f3cdd96.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/Alphapools.Finance/static/js/8.4f3cdd96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90753bfaf98f21a0b6ca",
+    "url": "/Alphapools.Finance/static/js/9.2e156473.chunk.js"
+  },
+  {
+    "revision": "96689ccaf7a747aacccc",
+    "url": "/Alphapools.Finance/static/js/main.4a2cc66e.chunk.js"
+  },
+  {
+    "revision": "e1adc16281833066d5a5",
+    "url": "/Alphapools.Finance/static/js/runtime-main.cf47305f.js"
+  }
+]);
